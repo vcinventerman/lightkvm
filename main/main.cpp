96b0d0c02dd4170cf333e0c59bf39daafdf82765
Nicholas Karr <nickkarr2005@gmail.com>
2024-01-06 12:17:59 -0600

    Fix descriptors for CDC/HID

--- main/main.cpp
+++ main/main.cpp
@@ -20,7 +20,6 @@
 
 extern "C" {
 
-#define TUSB_DESC_TOTAL_LEN      (TUD_CONFIG_DESC_LEN + CFG_TUD_HID * TUD_HID_DESC_LEN)
 
 const uint8_t hid_report_descriptor[] = {
     TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(HID_ITF_PROTOCOL_KEYBOARD))
@@ -32,21 +31,29 @@ const char* hid_string_descriptor[6] = {
     "TinyUSB",             // 1: Manufacturer
     "TinyUSB Device",      // 2: Product
     "123456",              // 3: Serials, should use chip ID
-    "Example HID interface",  // 4: HID
-    "Example CDC interface",  // 5: CDC
+    "Example CDC interface",  // 4: CDC
+    "Example HID interface",  // 5: HID
 };
+
+enum
+{
+  ITF_NUM_CDC = 0,
+  ITF_NUM_CDC_DATA,
+  ITF_NUM_HID,
+  ITF_NUM_TOTAL
+};
+
+#define TUSB_DESC_TOTAL_LEN      (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN + TUD_HID_DESC_LEN)
 
 static const uint8_t hid_configuration_descriptor[] = {
     // Configuration number, interface count, string index, total length, attribute, power in mA
-    TUD_CONFIG_DESCRIPTOR(1, 1, 0, TUSB_DESC_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
-
-    // Interface number, string index, boot protocol, report descriptor len, EP In address, size & polling interval
-    TUD_HID_DESCRIPTOR(0, 4, false, sizeof(hid_report_descriptor), 0x81, 16, 10),
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, TUSB_DESC_TOTAL_LEN, 0, 200),
 
     // Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-    //TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, EPNUM_CDC_NOTIF, 8, EPNUM_CDC_OUT, EPNUM_CDC_IN, 64),
-    //TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 5, 0x80 | EPNUM_0_CDC_NOTIF, 8, EPNUM_0_CDC, 0x80 | EPNUM_0_CDC, CFG_TUD_CDC_EP_BUFSIZE),
-    TUD_CDC_DESCRIPTOR(1, 5, 0x81, 8, 1, 0x81, CFG_TUD_CDC_EP_BUFSIZE),
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, 0x81, 8, 0x02, 0x82, CFG_TUD_CDC_EP_BUFSIZE),
+
+    // Interface number, string index, boot protocol, report descriptor len, EP In address, size & polling interval
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 5, false, sizeof(hid_report_descriptor), 0x83, 16, 10),
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR request
@@ -78,14 +85,35 @@ void app_main(void)
 
     printf("USB initialization\n");
 
-    const tinyusb_config_t tusb_cfg = {
-        .device_descriptor = NULL,
+    const static tusb_desc_device_t descriptor_config = {
+        .bLength = sizeof(tusb_desc_device_t),
+        .bDescriptorType = TUSB_DESC_DEVICE,
+        .bcdUSB = 0x0200,
+
+        // Use Interface Association Descriptor (IAD) for CDC
+        // As required by USB Specs IAD's subclass must be common class (2) and protocol must be IAD (1)
+        .bDeviceClass = TUSB_CLASS_MISC,
+        .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+        .bDeviceProtocol = MISC_PROTOCOL_IAD,
+
+        .bMaxPacketSize0 = 64,
+        .idVendor = 0x303A, // This is Espressif VID. This needs to be changed according to Users / Customers
+        .idProduct = 0x4002,
+        .bcdDevice = 0x100,
+        .iManufacturer = 0x01,
+        .iProduct = 0x02,
+        .iSerialNumber = 0x03,
+        .bNumConfigurations = 0x01
+    };
+
+    const static tinyusb_config_t tusb_cfg = {
+        .device_descriptor = &descriptor_config,
         .string_descriptor = hid_string_descriptor,
         .string_descriptor_count = sizeof(hid_string_descriptor) / sizeof(hid_string_descriptor[0]),
         .external_phy = false,
         .configuration_descriptor = hid_configuration_descriptor,
         .self_powered = false,
-        .vbus_monitor_io = -1
+        .vbus_monitor_io = 0
     };
 
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
